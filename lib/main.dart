74107dca696a3751aabf44f3e3b14e862f7d8db8
sanjula <myselfwebsites@gmail.com>
2025-09-24 14:15:16 +0530

    Upload a my current developping flutter Project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myapp());
+}
+
+class myapp extends StatelessWidget {
+  const myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Scaffold());
+  }
+}
